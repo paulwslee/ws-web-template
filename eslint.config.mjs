76c8f61ca0cc8ab266d04a099638116418ac4447
paulwslee <paul.ws.lee@icloud.com>
2025-11-07 12:34:50 -0500

    chore: test husky lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,39 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unicorn from 'eslint-plugin-unicorn';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  prettier, // prettier 충돌 방지용
 
-export default eslintConfig;
+  // 전역 ignore 설정
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+
+  // 추가 규칙
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      unicorn,
+      import: importPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prefer-const': 'warn',
+      'unicorn/filename-case': [
+        'warn',
+        {
+          cases: { camelCase: true, pascalCase: true, kebabCase: true },
+        },
+      ],
+      'import/no-unresolved': 'off',
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+    },
+  },
+]);
